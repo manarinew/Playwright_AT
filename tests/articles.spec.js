e5af44900294e2405aa36f03b184fc2801df6fd9
Elena Radoutskaya <elena.radoutskaya@lamoda.ru>
2025-11-24 23:49:02 +0300

    домашка 1 промежуточное

--- tests/articles.spec.js
+++ tests/articles.spec.js
@@ -2,12 +2,12 @@ import { test, expect } from '@playwright/test';
 
 const apiUrl = 'https://realworld.qa.guru/api';
 
-test('Неавторизованный может получить список статей', async ({ request }) => {
+test('Неавторизованный пользователь может получить список статей', async ({ request }) => {
     const response = await request.get('https://realworld.qa.guru/api/articles');
     expect(response.status()).toBe(200);
 });
 
-test('Неавторизованный может получить список статей с пагинацией', async ({ request }) => {
+test('Неавторизованный пользователь может получить список статей с пагинацией', async ({ request }) => {
     const response = await request.get('https://realworld.qa.guru/api/articles?limit=3&&offset=0');
     expect(response.status()).toBe(200);
     console.log(response.headers());
--- tests/homework1.spec.js
+++ tests/homework1.spec.js
@@ -1,30 +1,51 @@
 import { test, expect } from '@playwright/test';
-import { faker } from "@faker-js/faker";
+import { faker } from '@faker-js/faker';
 
 //ДЗ:
 //Написать тесты на сайт на каждый метод
 // https://apichallenges.herokuapp.com/gui/challenges/7d9e2ef3-dfd2-48e0-ac9f-5fa97f575f7d
-const apiUrl = 'https://apichallenges.eviltester.com'
-let token;
+const apiUrl = 'https://apichallenges.eviltester.com';
+let token; //токен для прогресса тестов
+let taskId; // переменная для id задачи (так как в списке задач id видимо периодически обновляются)
+let headers;
 
-test.describe("Challenge", () => {
-    test.beforeAll(async ({ request }) => {
-        const response = await request.post(`${apiUrl}/challenger`);
-        const headers = response.headers();
-        token = headers['x-challenger'];
-        console.log(`!!тут ссылку с токеном вставить!!`);
+test.describe.only("Challenge", () => {
+    //получаем токен
+    test.beforeAll (async ({ request }) => {
+        let response = await request.post(`${apiUrl}/challenger`);
+        const headersForToken = response.headers();
+        token = headersForToken['x-challenger'];
+        headers = {'X-Challenger': token};
+        console.log(`https://apichallenges.herokuapp.com/gui/challenges/${token}`);
+        //получаем id для 5 теста
+        response = await request.get(`${apiUrl}/todos`,{headers});
+        const body = await response.json();
+        taskId = body.todos[0].id;
     });
 
-const apiUrl = 'https://realworld.qa.guru/api';
+    test ('02. Получить список всех челленджей', async({request}) => {
+        const response = await request.get(`${apiUrl}/challenges`, {headers});
+        const body = await response.json();
+        expect(response.status()).toBe(200);
+        expect(body.challenges[1].status).toBe(true);
+    });
 
-test('Пользователь может зарегистрироваться используя email и пароль', async ({ request }) => {
-    const response = await request.post(`${apiUrl}/challenges`, {
-        headers:{
-            'x-challenger': token
+    test ('05. Успешно получить таску по id', async({request}) => {
+        console.log(`Номер таски ${taskId}`);
+        const response = await request.get(`${apiUrl}/todos/${taskId}`, {headers});
+        expect(response.ok()).toBeTruthy();
+        //посмотрим, что имя задачи не пустое
+        const body = await response.json();
+        expect(body.todos[0].title).not.toBe('');
+    });
+
+    test('09. Успешное создание таски', async ({request}) => {
+        const data = {
+            "title": faker.lorem.word(5),
+                "doneStatus": false,
+                "description": faker.lorem.sentence({min: 3, max: 5})
         }
-    });
-    const body = await response.json();
-    expect(body.challenges.length).toBe(59);
-
-});
+    const response = await request.post(`${apiUrl}/todos`, {headers}, {data});
+    console.log(data);
+    })
 })
